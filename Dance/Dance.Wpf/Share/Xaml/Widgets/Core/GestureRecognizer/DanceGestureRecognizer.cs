97fc368aaae542f5c734af0342c336d2a5491d26
240829120 <240829120@qq.com>
2023-08-15 13:30:55 +0800

    添加WPF手势控制

---
+++ Dance/Dance.Wpf/Share/Xaml/Widgets/Core/GestureRecognizer/DanceGestureRecognizer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Dance.Wpf
+{
+    /// <summary>
+    /// 手势识别控件
+    /// </summary>
+    public class DanceGestureRecognizer : ContentControl
+    {
+        #region GestureRecognizers -- 手势识别器集合
+
+        /// <summary>
+        /// 手势识别器集合
+        /// </summary>
+        public IList<IDanceGestureRecognizer> GestureRecognizers
+        {
+            get { return (IList<IDanceGestureRecognizer>)GetValue(GestureRecognizersProperty); }
+            set { SetValue(GestureRecognizersProperty, value); }
+        }
+
+        /// <summary>
+        /// 手势识别器集合
+        /// </summary>
+        public static readonly DependencyProperty GestureRecognizersProperty =
+            DependencyProperty.Register("GestureRecognizers", typeof(IList<IDanceGestureRecognizer>), typeof(DanceGestureRecognizer), new PropertyMetadata(null));
+
+        #endregion
+    }
+}
---
+++ Dance/Dance.Wpf/Share/Xaml/Widgets/Core/GestureRecognizer/IDanceGestureRecognizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dance.Wpf
+{
+    /// <summary>
+    /// 手势识别
+    /// </summary>
+    public interface IDanceGestureRecognizer : IDisposable
+    {
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="element">元素</param>
+        void Register(FrameworkElement element);
+
+        /// <summary>
+        /// 注销
+        /// </summary>
+        /// <param name="element">元素</param>
+        void UnRegister(FrameworkElement element);
+    }
+}
