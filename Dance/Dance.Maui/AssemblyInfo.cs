f6ab91507beede4bf13e0a2263f10fec2926f0ea
240829120 <240829120@qq.com>
2023-08-31 17:28:37 +0800

    统一命名空间

---
+++ Dance/Dance.Maui/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://schemas.dance.com/maui", "Dance")]
+[assembly: XmlnsDefinition("http://schemas.dance.com/maui", "Dance.Maui")]
