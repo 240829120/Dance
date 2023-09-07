03d41b865778ca1a31d6a59e610ad0cf4b8369f7
240829120 <240829120@qq.com>
2023-09-07 17:28:20 +0800

    窗口统一样式

---
+++ Dance/Dance.Wpf/Share/Xaml/Widgets/WindowPart/DanceWindowClosePart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Dance.Wpf
+{
+    /// <summary>
+    /// 窗口关闭部件
+    /// </summary>
+    public class DanceWindowClosePart : Button
+    {
+        static DanceWindowClosePart()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DanceWindowClosePart), new FrameworkPropertyMetadata(typeof(DanceWindowClosePart)));
+        }
+
+        /// <summary>
+        /// 点击
+        /// </summary>
+        protected override void OnClick()
+        {
+            base.OnClick();
+
+            if (Window.GetWindow(this) is not Window window)
+                return;
+
+            window.Close();
+        }
+    }
+}
---
+++ Dance/Dance.Wpf/Share/Xaml/Widgets/WindowPart/DanceWindowMinPart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Dance.Wpf
+{
+    /// <summary>
+    /// 窗口最小化部件
+    /// </summary>
+    public class DanceWindowMinPart : Button
+    {
+        static DanceWindowMinPart()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DanceWindowMinPart), new FrameworkPropertyMetadata(typeof(DanceWindowMinPart)));
+        }
+
+        /// <summary>
+        /// 点击
+        /// </summary>
+        protected override void OnClick()
+        {
+            base.OnClick();
+
+            if (Window.GetWindow(this) is not Window window)
+                return;
+
+            window.WindowState = WindowState.Minimized;
+        }
+    }
+}
