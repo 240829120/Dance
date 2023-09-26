c066244857d3c1e91abdaa82935c26ad0bc98877
240829120 <240829120@qq.com>
2023-09-26 18:34:07 +0800

    添加消息框图标

---
+++ Dance/Dance.Wpf/Share/Xaml/DanceResourceIcons.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Dance.Wpf
+{
+    /// <summary>
+    /// 图标资源
+    /// </summary>
+    public static class DanceResourceIcons
+    {
+        /// <summary>
+        /// 失败
+        /// </summary>
+        public static ImageSource Failure { get; } = new BitmapImage(new Uri("/Dance.Wpf;component/Themes/Resources/Icon/failure.png", UriKind.RelativeOrAbsolute));
+
+        /// <summary>
+        /// 成功
+        /// </summary>
+        public static ImageSource Success { get; } = new BitmapImage(new Uri("/Dance.Wpf;component/Themes/Resources/Icon/success.png", UriKind.RelativeOrAbsolute));
+
+        /// <summary>
+        /// 警告
+        /// </summary>
+        public static ImageSource Warning { get; } = new BitmapImage(new Uri("/Dance.Wpf;component/Themes/Resources/Icon/warning.png", UriKind.RelativeOrAbsolute));
+    }
+}
