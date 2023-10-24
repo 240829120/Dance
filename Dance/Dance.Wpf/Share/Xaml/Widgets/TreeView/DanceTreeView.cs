f6ba8941a2b30dfca9dbe64c6a9faf3acce51071
240829120 <240829120@qq.com>
2023-10-24 18:41:30 +0800

    多选支持

--- Dance/Dance.Wpf/Share/Xaml/Widgets/TreeView/DanceTreeView.cs
+++ Dance/Dance.Wpf/Share/Xaml/Widgets/TreeView/DanceTreeView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,83 @@ namespace Dance.Wpf
             DefaultStyleKeyProperty.OverrideMetadata(typeof(DanceTreeView), new FrameworkPropertyMetadata(typeof(DanceTreeView)));
         }
 
+        // =======================================================================================
+        // Field
+
+        /// <summary>
+        /// 选中的项集合
+        /// </summary>
+        internal List<DanceTreeViewItem> SelectedItems = new();
+
+        // =======================================================================================
+        // Property
+
+        #region IsMultiSelectedEnabled -- 是否启用多选
+
+        /// <summary>
+        /// 是否启用多选
+        /// </summary>
+        public bool IsMultiSelectedEnabled
+        {
+            get { return (bool)GetValue(IsMultiSelectedEnabledProperty); }
+            set { SetValue(IsMultiSelectedEnabledProperty, value); }
+        }
+
+        /// <summary>
+        /// 是否启用多选
+        /// </summary>
+        public static readonly DependencyProperty IsMultiSelectedEnabledProperty =
+            DependencyProperty.Register("IsMultiSelectedEnabled", typeof(bool), typeof(DanceTreeView), new PropertyMetadata(false));
+
+        #endregion
+
+        #region SlectedValue -- 当前选中项
+
+        /// <summary>
+        /// 当前选中项
+        /// </summary>
+        public object SlectedValue
+        {
+            get { return (object)GetValue(SlectedValueProperty); }
+            set { SetValue(SlectedValueProperty, value); }
+        }
+
+        /// <summary>
+        /// 当前选中项
+        /// </summary>
+        public static readonly DependencyProperty SlectedValueProperty =
+            DependencyProperty.Register("SlectedValue", typeof(object), typeof(DanceTreeView), new PropertyMetadata(null));
+
+        #endregion
+
+        #region SlectedValues -- 当前选中项集合
+
+        /// <summary>
+        /// 当前选中项集合
+        /// </summary>
+        public IList SelectedValues
+        {
+            get { return (IList)GetValue(SelectedValuesProperty); }
+            set { SetValue(SelectedValuesProperty, value); }
+        }
+
+        /// <summary>
+        /// 当前选中项集合
+        /// </summary>
+        public static readonly DependencyProperty SelectedValuesProperty =
+            DependencyProperty.Register("SelectedValues", typeof(IList), typeof(DanceTreeView), new PropertyMetadata(null));
+
+        #endregion
+
+        // =======================================================================================
+        // Override Function
+
         protected override bool IsItemItsOwnContainerOverride(object item)
         {
             if (item is DanceTreeViewItem node)
             {
                 node.Level = 1;
+                node.TreeView = this;
                 return true;
             }
 
@@ -31,7 +104,7 @@ namespace Dance.Wpf
 
         protected override DependencyObject GetContainerForItemOverride()
         {
-            return new DanceTreeViewItem() { Level = 1 };
+            return new DanceTreeViewItem() { Level = 1, TreeView = this };
         }
     }
 }
--- Dance/Dance.Wpf/Share/Xaml/Widgets/TreeView/DanceTreeViewItem.cs
+++ Dance/Dance.Wpf/Share/Xaml/Widgets/TreeView/DanceTreeViewItem.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Dance.Wpf
 {
@@ -17,6 +18,11 @@ namespace Dance.Wpf
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(DanceTreeViewItem), new FrameworkPropertyMetadata(typeof(DanceTreeViewItem)));
         }
+
+        /// <summary>
+        /// 所属树
+        /// </summary>
+        internal DanceTreeView? TreeView { get; set; }
 
         #region Level -- 等级
 
@@ -37,11 +43,31 @@ namespace Dance.Wpf
 
         #endregion
 
+        #region IsMultiSelected -- 是否被多选
+
+        /// <summary>
+        /// 是否被多选
+        /// </summary>
+        public bool IsMultiSelected
+        {
+            get { return (bool)GetValue(IsMultiSelectedProperty); }
+            set { SetValue(IsMultiSelectedProperty, value); }
+        }
+
+        /// <summary>
+        /// 是否被多选
+        /// </summary>
+        public static readonly DependencyProperty IsMultiSelectedProperty =
+            DependencyProperty.Register("IsMultiSelected", typeof(bool), typeof(DanceTreeViewItem), new PropertyMetadata(false));
+
+        #endregion
+
         protected override bool IsItemItsOwnContainerOverride(object item)
         {
             if (item is DanceTreeViewItem node)
             {
                 node.Level = this.Level + 1;
+                node.TreeView = this.TreeView;
                 return true;
             }
 
@@ -50,7 +76,54 @@ namespace Dance.Wpf
 
         protected override DependencyObject GetContainerForItemOverride()
         {
-            return new DanceTreeViewItem() { Level = this.Level + 1 };
+            return new DanceTreeViewItem() { Level = this.Level + 1, TreeView = this.TreeView };
+        }
+
+        /// <summary>
+        /// 鼠标按下
+        /// </summary>
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+
+            if (this.TreeView == null || e.OriginalSource is not FrameworkElement element || DanceXamlExpansion.GetVisualTreeParent<DanceTreeViewItem>(element) != this)
+                return;
+
+            if (Keyboard.Modifiers.HasFlag(ModifierKeys.Control))
+            {
+                this.IsMultiSelected = !this.IsMultiSelected;
+
+                if (this.IsMultiSelected)
+                {
+                    this.TreeView.SelectedItems.Add(this);
+                }
+                else
+                {
+                    this.TreeView.SelectedItems.Remove(this);
+                }
+            }
+            else if (Keyboard.Modifiers.HasFlag(ModifierKeys.Shift))
+            {
+                DanceTreeViewItem? from = this.TreeView.SelectedItems.LastOrDefault();
+                if (from == null)
+                {
+                    this.IsMultiSelected = true;
+                    this.TreeView.SelectedItems.Clear();
+                    this.TreeView.SelectedItems.Add(this);
+
+                    return;
+                }
+
+
+            }
+            else
+            {
+                this.TreeView.SelectedItems.ForEach(p => p.IsMultiSelected = false);
+
+                this.IsMultiSelected = true;
+                this.TreeView.SelectedItems.Clear();
+                this.TreeView.SelectedItems.Add(this);
+            }
         }
     }
 }
