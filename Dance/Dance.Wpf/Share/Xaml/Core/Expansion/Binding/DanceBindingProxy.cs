baf0c249271828b06de1f366bb6e2eaf5264292f
240829120 <240829120@qq.com>
2023-11-03 15:18:35 +0800

    连接分组操作

---
+++ Dance/Dance.Wpf/Share/Xaml/Core/Expansion/Binding/DanceBindingProxy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dance.Wpf
+{
+    /// <summary>
+    /// 绑定代理
+    /// </summary>
+    public class DanceBindingProxy : Freezable
+    {
+        #region Source -- 源
+
+        /// <summary>
+        /// 源
+        /// </summary>
+        public object Source
+        {
+            get { return (object)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        /// <summary>
+        /// 源
+        /// </summary>
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(object), typeof(DanceBindingProxy), new PropertyMetadata(null));
+
+        #endregion
+
+        protected override Freezable CreateInstanceCore() => new DanceBindingProxy();
+    }
+}
