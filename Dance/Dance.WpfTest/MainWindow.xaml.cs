384851abb75516cc95fc1891009f0f2c9818d1f3
240829120 <240829120@qq.com>
2023-11-09 18:39:56 +0800

    集成 WPFToolKit

--- Dance/Dance.WpfTest/MainWindow.xaml.cs
+++ Dance/Dance.WpfTest/MainWindow.xaml.cs
@@ -23,49 +23,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
 
 namespace Dance.WpfTest
 {
-    public class TestItem : DanceModel
+    public class Student
     {
+        [Category("基础")]
         public string? Name { get; set; }
-    }
-
-    public class TestGroup : DanceModel
-    {
-        public string? Name { get; set; }
-
-        public List<TestItem>? Items { get; set; }
-    }
-
-    public class MainWindowModel : DanceViewModel
-    {
-        public MainWindowModel()
-        {
-            this.DragBeginCommand = new RelayCommand<DanceDragBeginEventArgs>(this.DragBegin);
-            this.DropCommand = new RelayCommand<DanceDragEventArgs>(this.Drop);
-        }
-
-        public RelayCommand<DanceDragBeginEventArgs> DragBeginCommand { get; set; }
-
-        private void DragBegin(DanceDragBeginEventArgs? e)
-        {
-            if (e == null)
-                return;
-
-            e.Data = "this is a try.";
-        }
-
-        public RelayCommand<DanceDragEventArgs> DropCommand { get; set; }
-
-        private void Drop(DanceDragEventArgs? e)
-        {
-            if (e == null)
-                return;
-
-            string? data = e.EventArgs.Data.GetData(typeof(string))?.ToString();
-            DanceMessageExpansion.ShowNotify(ToolTipIcon.Info, "测试", data ?? string.Empty);
-        }
     }
 
     /// <summary>
@@ -77,32 +42,13 @@ namespace Dance.WpfTest
         {
             InitializeComponent();
 
-            this.DataContext = new MainWindowModel();
-
             this.Loaded += MainWindow_Loaded;
             this.Closed += MainWindow_Closed;
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            List<TestGroup> groups = new();
-            groups.Add(new TestGroup { Name = "Group 1" });
-            groups.Add(new TestGroup { Name = "Group 2" });
-            groups.Add(new TestGroup { Name = "Group 3" });
-
-            groups[0].Items = new List<TestItem>();
-            groups[0].Items.Add(new TestItem { Name = "Item 1 -- 1" });
-            groups[0].Items.Add(new TestItem { Name = "Item 1 -- 2" });
-
-            groups[1].Items = new List<TestItem>();
-            groups[1].Items.Add(new TestItem { Name = "Item 2 -- 1" });
-            groups[1].Items.Add(new TestItem { Name = "Item 2 -- 2" });
-
-            groups[2].Items = new List<TestItem>();
-            groups[2].Items.Add(new TestItem { Name = "Item 3 -- 1" });
-            groups[2].Items.Add(new TestItem { Name = "Item 3 -- 2" });
-
-            this.groupBox.ItemsSource = groups;
+            this.propertyGrid.SelectedObject = new Student();
         }
 
         private void MainWindow_Closed(object? sender, EventArgs e)
