642c38d9ee2830216cd1bd0995727a34c3ade70a
240829120 <240829120@qq.com>
2023-11-16 17:43:34 +0800

    修复 PropertyGrid缩进报错

---
+++ Dance/Dance.Wpf/Themes/Toolkit/DanceToolkitPropertyGridIndent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Xceed.Wpf.Toolkit.PropertyGrid;
+
+namespace Dance.Wpf
+{
+    /// <summary>
+    /// 属性列表缩进
+    /// </summary>
+    public class DanceToolkitPropertyGridIndent : Border
+    {
+        public DanceToolkitPropertyGridIndent()
+        {
+            this.Loaded += DanceToolkitPropertyGridIndent_Loaded;
+        }
+
+        #region UnitWidth -- 单位宽度
+
+        /// <summary>
+        /// 单位宽度
+        /// </summary>
+        public double UnitWidth
+        {
+            get { return (double)GetValue(UnitWidthProperty); }
+            set { SetValue(UnitWidthProperty, value); }
+        }
+
+        /// <summary>
+        /// 单位宽度
+        /// </summary>
+        public static readonly DependencyProperty UnitWidthProperty =
+            DependencyProperty.Register("UnitWidth", typeof(double), typeof(DanceToolkitPropertyGridIndent), new PropertyMetadata(15d));
+
+        #endregion
+
+        private void DanceToolkitPropertyGridIndent_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            PropertyItemBase? parent = DanceXamlExpansion.GetVisualTreeParent<PropertyItemBase>(this);
+            if (parent == null)
+                return;
+
+            this.Width = (parent.Level + 1) * this.UnitWidth;
+        }
+    }
+}
